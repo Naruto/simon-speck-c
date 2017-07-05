0bbe930145f6df61389d1768269b076e2799a787
Naruto TAKAHASHI <naruto.takahashi@dena.com>
2017-07-05 15:31:24 +0900

    support stl types that is stlport, gnustl, c++

--- scripts/speck/build_android.sh
+++ scripts/speck/build_android.sh
@@ -12,18 +12,26 @@ ARCHTECTURES=(
 #    "arm64-v8a"
 )
 
+STL_TYPES=(
+    "stlport"
+    "c++"
+    "gnustl"
+)
+
 pushd ${BASEDIR} > /dev/null
 for arch in ${ARCHTECTURES[@]}; do
     # build
-    /bin/rm -rf build
-    /bin/mkdir build
-    pushd build > /dev/null
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=16 -DCMAKE_ANDROID_ARCH_ABI=${arch} -DCMAKE_ANDROID_NDK=${NDK_ROOT} -DCMAKE_ANDROID_STL_TYPE=gnustl_static ..
-    make
-    popd > /dev/null
+    for stl_type in ${STL_TYPES[@]}; do
+        /bin/rm -rf build
+        /bin/mkdir build
+        pushd build > /dev/null
+        cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=16 -DCMAKE_ANDROID_ARCH_ABI=${arch} -DCMAKE_ANDROID_NDK=${NDK_ROOT} -DCMAKE_ANDROID_STL_TYPE=${stl_type}_static ..
+        make
+        popd > /dev/null
+        # deploy
+        /bin/mkdir -p libs/android/${arch}/${stl_type}
+        /bin/cp build/libspeck.so libs/android/${arch}/${stl_type}
+    done
 
-    # deploy
-    /bin/mkdir -p libs/android/${arch}
-    /bin/cp build/libspeck.so libs/android/${arch}
 done
 popd > /dev/null
