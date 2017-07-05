cae68105ebb2688b54f2badb4c1ae6d24f6297d3
Naruto TAKAHASHI <naruto.takahashi@dena.com>
2017-07-05 19:06:43 +0900

    check NDK_ROOT or ANDROID_NDK_HOME variables

--- scripts/speck/build_android.sh
+++ scripts/speck/build_android.sh
@@ -18,6 +18,16 @@ STL_TYPES=(
     "gnustl"
 )
 
+NDK=
+if [ x$NDK_ROOT != x"" ]; then
+    NDK=${NDK_ROOT}
+elif [ x$ANDROID_NDK_HOME != x"" ]; then
+    NDK=${ANDROID_NDK_HOME}
+else
+    echo "please set Android NDK path to 'NDK_ROOT' or 'ANDROID_NDK_HOME' environment variables"
+    exit 1
+fi
+
 pushd ${BASEDIR} > /dev/null
 for arch in ${ARCHTECTURES[@]}; do
     # build
@@ -25,8 +35,9 @@ for arch in ${ARCHTECTURES[@]}; do
         /bin/rm -rf build
         /bin/mkdir build
         pushd build > /dev/null
-        cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=16 -DCMAKE_ANDROID_ARCH_ABI=${arch} -DCMAKE_ANDROID_NDK=${NDK_ROOT} -DCMAKE_ANDROID_STL_TYPE=${stl_type}_static ..
+        cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=16 -DCMAKE_ANDROID_ARCH_ABI=${arch} -DCMAKE_ANDROID_NDK=${NDK} -DCMAKE_ANDROID_STL_TYPE=${stl_type}_static ..
         make
+        # TODO: strip
         popd > /dev/null
         # deploy
         /bin/mkdir -p libs/android/${arch}/${stl_type}
