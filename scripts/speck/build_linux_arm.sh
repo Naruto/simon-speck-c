5ca58bf6f007c0eb03e04ca787e9704a0f7ec5f0
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-07-22 11:46:47 +0900

    add arm and aarch64 build script

--- scripts/speck/build_linux_arm.sh
+++ scripts/speck/build_linux_arm.sh
@@ -10,7 +10,7 @@ pushd ${BASEDIR} > /dev/null
 /bin/mkdir build
 # build
 pushd build > /dev/null
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++ -DENABLE_NEON=ON ..
+cmake -DCMAKE_CROSSCOMPILING=ON -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_VERSION=1  -DCMAKE_SYSTEM_PROCESSOR=arm -DCMAKE_CROSSCOMPILING_EMULATOR=${BASEDIR}/scripts/tools/ubuntu/run_qemu_arm.sh -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++ -DENABLE_NEON=ON -DENABLE_TESTING=ON ..
 make VERBOSE=1
 ctest
 popd > /dev/null
