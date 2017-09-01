76ad02f8d9a99492bf1df77a29424d7fefd65bc7
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-09-01 23:33:07 +0900

    enable avx2 on windows x86_64

--- scripts/speck/build_win.bat
+++ scripts/speck/build_win.bat
@@ -22,6 +22,6 @@ set OUTPUTDIR=%PROJDIR%output\win64\
 rmdir /S/Q %BUILDDIR%
 mkdir %BUILDDIR%
 cd %BUILDDIR%
-cmake %PROJDIR% -G"Visual Studio 14 2015 Win64" -DBUILD_SHARED_LIBS=ON -DCMAKE_CONFIGURATION_TYPES=Release
+cmake %PROJDIR% -G"Visual Studio 14 2015 Win64" -DBUILD_SHARED_LIBS=ON -DENABLE_AVX2=ON -DCMAKE_CONFIGURATION_TYPES=Release
 %MSBUILDBIN% ALL_BUILD.vcxproj /property:Configuration=Release
 cd ..
