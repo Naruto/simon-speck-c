822618d9767918d5ce8c4b5f1941242beff2162a
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-09-03 00:39:21 +0900

    improve speck_test code

--- src/speck/speck_test.cpp
+++ src/speck/speck_test.cpp
@@ -3,100 +3,156 @@
 #include <string.h>
 #include <cstdlib>
 
+// https://eprint.iacr.org/2013/404.pdf
+//
+// Speck128/128
+//  Key:        0f0e0d0c0b0a0908 0706050403020100
+//  Plaintext:  6c61766975716520 7469206564616d20
+//  Ciphertext: a65d985179783265 7860fedf5c570d18
+static const uint64_t s_key[2] = {0x0706050403020100, 0x0f0e0d0c0b0a0908};
+static const uint64_t s_plain_text[2] = {0x7469206564616d20, 0x6c61766975716520};
+static const uint64_t s_cipher_text[2] = {0x7860fedf5c570d18, 0xa65d985179783265};
+static const uint8_t s_plain_text_stream[16] = {
+        0x20, 0x6d, 0x61, 0x64, 0x65, 0x20, 0x69, 0x74, 0x20, 0x65, 0x71, 0x75, 0x69, 0x76, 0x61, 0x6c,
+};
+static const uint8_t s_cipher_text_stream[16] = {
+        0x18, 0x0d, 0x57, 0x5c, 0xdf, 0xfe, 0x60, 0x78, 0x65, 0x32, 0x78, 0x79, 0x51, 0x98, 0x5d, 0xa6,
+};
+
 int main() {
-    uint64_t key[2];
-    key[0] = 0x0706050403020100;
-    key[1] = 0x0f0e0d0c0b0a0908;
 
-    speck_ctx_t *ctx = speck_init(SPECK_ENCRYPT_TYPE_128_128, SPECK_BLOCK_CIPHER_MODE_ECB, key);
-    if(!ctx) return 1;
-
+    // block encrypt & decrypt
     {
-      uint64_t plain_text[2];
-      uint64_t cipher_text[2];
-      uint64_t tmp[2];
+        speck_ctx_t *ctx = speck_init(SPECK_ENCRYPT_TYPE_128_128, SPECK_BLOCK_CIPHER_MODE_ECB, s_key);
+        if(!ctx) return 1;
 
-      plain_text[0] = 0x7469206564616d20;
-      plain_text[1] = 0x6c61766975716520;
+        uint64_t plain_text[2];
+        uint64_t cipher_text[2];
+        uint64_t decrypted_text[2];
 
+        plain_text[0] = 0x7469206564616d20;
+        plain_text[1] = 0x6c61766975716520;
 
-      speck_encrypt(ctx, plain_text, cipher_text);
-      printf("0x%llx\n", cipher_text[0]);
-      printf("0x%llx\n", cipher_text[1]);
+        printf("ECB block encrypt decrypt\n");
 
-      speck_decrypt(ctx, cipher_text, tmp);
-      printf("0x%llx\n", tmp[0]);
-      printf("0x%llx\n", tmp[1]);
+        speck_encrypt(ctx, plain_text, cipher_text);
+        printf("%20s 0x%llx 0x%llx\n", "encrypted : ", cipher_text[1], cipher_text[0]);
+        if(!(cipher_text[0] == s_cipher_text[0] && cipher_text[1] == s_cipher_text[1])) {
+            return 1;
+        }
+
+        speck_decrypt(ctx, cipher_text, decrypted_text);
+        printf("%20s 0x%llx 0x%llx\n", "decrypted : ", decrypted_text[1], decrypted_text[0]);
+        if(!(decrypted_text[0] == s_plain_text[0] && decrypted_text[1] == s_plain_text[1])) {
+            return 1;
+        }
+
+        printf("\n");
+
+        speck_finish(&ctx);
     }
 
+    // ECB stream encrypt & decrypt
     {
-        int siz = 16;
-      unsigned char *plain_text = (unsigned char*) calloc(1, siz);
-      unsigned char *crypted_text = (unsigned char*) calloc(1, siz);
-      unsigned char *tmp = (unsigned char*) calloc(1, siz);
+        speck_ctx_t *ctx = speck_init(SPECK_ENCRYPT_TYPE_128_128, SPECK_BLOCK_CIPHER_MODE_ECB, s_key);
+        if(!ctx) return 1;
 
-      plain_text[0] = 0x20;
-      plain_text[1] = 0x6D;
-      plain_text[2] = 0x61;
-      plain_text[3] = 0x64;
+        int i;
+        size_t siz = 16;
+        unsigned char *plain_text = (unsigned char*) calloc(1, siz);
+        unsigned char *crypted_text = (unsigned char*) calloc(1, siz);
+        unsigned char *decrypted_text = (unsigned char*) calloc(1, siz);
 
-      plain_text[4] = 0x65;
-      plain_text[5] = 0x20;
-      plain_text[6] = 0x69;
-      plain_text[7] = 0x74;
+        memcpy(plain_text, s_plain_text_stream, sizeof(s_cipher_text_stream));
 
-      plain_text[8] = 0x20;
-      plain_text[9] = 0x65;
-      plain_text[10] = 0x71;
-      plain_text[11] = 0x75;
+        printf("ECB stream ph1\n");
 
-      plain_text[12] = 0x69;
-      plain_text[13] = 0x76;
-      plain_text[14] = 0x61;
-      plain_text[15] = 0x6C;
+        printf("%20s ", "plain text :");
+        for(i=siz-1;i >=0;i--)
+            printf("%02x ", plain_text[i]);
+        printf("\n");
 
-      speck_encrypt_ex(ctx, plain_text, crypted_text, siz);
-      for(int i=siz-1;i >=0;i--)
-        printf("%02x", crypted_text[i]);
-      printf("\n");
+        speck_encrypt_ex(ctx, plain_text, crypted_text, siz);
+        printf("%20s ", "encrypted text :");
+        for(i=siz-1;i >=0;i--)
+            printf("%02x ", crypted_text[i]);
+        printf("\n");
+        // check
+        for (int i = 0; i < siz; i++) {
+            if (s_cipher_text_stream[i] != crypted_text[i]) {
+                printf("encrypted error idx:%d  0x%02x != 0x%02x\n", i, s_cipher_text_stream[i], crypted_text[i]);
+                return 1;
+            }
+        }
 
-      speck_decrypt_ex(ctx, crypted_text, tmp, siz);
-      for(int i=siz-1;i >=0;i--)
-        printf("%02x", tmp[i]);
-      printf("\n");
+        speck_decrypt_ex(ctx, crypted_text, decrypted_text, siz);
+        printf("%20s ", "decrypted text :");
+        for(int i=siz-1;i >=0;i--)
+            printf("%02x ", decrypted_text[i]);
+        printf("\n");
 
-        free(tmp);
+        // check
+        for (int i = 0; i < siz; i++) {
+            if (s_plain_text_stream[i] != decrypted_text[i]) {
+                printf("decrypted error idx:%d  0x%02x != 0x%02x\n", i, s_plain_text_stream[i], decrypted_text[i]);
+                return 1;
+            }
+        }
+        printf("\n");
+
+        free(decrypted_text);
         free(crypted_text);
         free(plain_text);
+
+        speck_finish(&ctx);
     }
 
+    // ECB stream encrypt & decrypt ph2
     {
-        int siz = 128;
-      int i;
-      unsigned char *plain_text = (unsigned char*)calloc(1, siz);
-      unsigned char *crypted_text = (unsigned char*)calloc(1, siz);
-      unsigned char *tmp_text = (unsigned char*)calloc(1, siz);
+        speck_ctx_t *ctx = speck_init(SPECK_ENCRYPT_TYPE_128_128, SPECK_BLOCK_CIPHER_MODE_ECB, s_key);
+        if(!ctx) return 1;
 
-      strcpy((char*)plain_text, "abcdefghijklmnopqrstyvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-      for(i=siz-1;i>=0;i--)
-        printf("%02x ", plain_text[i]);
-      printf("\n");
+        size_t siz = 128;
+        int i;
+        unsigned char *plain_text = (unsigned char*)calloc(1, siz);
+        unsigned char *crypted_text = (unsigned char*)calloc(1, siz);
+        unsigned char *decrypted_text = (unsigned char*)calloc(1, siz);
 
-      speck_encrypt_ex(ctx, plain_text, crypted_text, siz);
-      for(i=siz-1;i>=0;i--)
-        printf("%02x ", crypted_text[i]);
-      printf("\n");
+        printf("ECB stream ph2\n");
 
-      speck_decrypt_ex(ctx, crypted_text, tmp_text, siz);
-      for(i=siz-1;i>=0;i--)
-        printf("%02x ", tmp_text[i]);
-      printf("\n");
+        memcpy(plain_text, "abcdefghijklmnopqrstyvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", siz);
+        printf("%20s ", "plain text :");
+        for(i=siz-1;i>=0;i--)
+            printf("%02x ", plain_text[i]);
+        printf("\n");
 
-        free(tmp_text);
+        speck_encrypt_ex(ctx, plain_text, crypted_text, siz);
+        printf("%20s ", "encrypted text :");
+        for(i=siz-1;i>=0;i--)
+            printf("%02x ", crypted_text[i]);
+        printf("\n");
+
+        speck_decrypt_ex(ctx, crypted_text, decrypted_text, siz);
+        printf("%20s ", "decrypted text :");
+        for(i=siz-1;i>=0;i--)
+            printf("%02x ", decrypted_text[i]);
+        printf("\n");
+
+        // check
+        for (int i = 0; i < siz; i++) {
+            if (plain_text[i] != decrypted_text[i]) {
+                printf("decrypted error idx:%d  0x%02x != 0x%02x\n", i, plain_text[i], decrypted_text[i]);
+                return 1;
+            }
+        }
+        printf("\n");
+
+        free(decrypted_text);
         free(crypted_text);
         free(plain_text);
+
+        speck_finish(&ctx);
     }
 
-    speck_finish(&ctx);
     return 0;
 }
