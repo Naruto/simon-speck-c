65667ac5dd44a3063457179a8311555accc95e83
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-09-18 18:14:57 +0900

    fix

--- scripts/speck/build_win.bat
+++ scripts/speck/build_win.bat
@@ -3,21 +3,28 @@ setlocal
 
 set SCRIPTDIR=%~dp0
 set PROJDIR=%SCRIPTDIR%..\..\
+set LIBSDIR=%PROJDIR%libs\
 set MSBUILDBIN="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 set ORIGPATH=%PATH%
 
 cd %PROJDIR%
 
 @echo on
+
+rmdir /S/Q %LIBSDIR%windows
+mkdir %LIBSDIR%
+mkdir %LIBSDIR%windows
+
 set BUILDDIR=%PROJDIR%build_win32\
-set OUTPUTDIR=%PROJDIR%output\win32\
+set OUTPUTDIR=%LIBSDIR%windows\win32\
 rmdir /S/Q %BUILDDIR%
 mkdir %BUILDDIR%
 cd %BUILDDIR%
-cmake %PROJDIR% -G"Visual Studio 14 2015" -DENABLE_TESTING=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_CONFIGURATION_TYPES=Release
+cmake %PROJDIR% -G"Visual Studio 14 2015" -DBUILD_SHARED_LIBS=ON -DCMAKE_CONFIGURATION_TYPES=Release
 %MSBUILDBIN% ALL_BUILD.vcxproj /property:Configuration=Release
 set PATH=%PROJDIR%build_win32/Release;%PATH%
-ctest .
+mkdir %OUTPUTDIR%
+copy %BUILDDIR%Release\speck.dll %OUTPUTDIR%
 cd ..
 
 set BUILDDIR=%PROJDIR%build_win64\
@@ -25,8 +32,9 @@ set OUTPUTDIR=%PROJDIR%output\win64\
 rmdir /S/Q %BUILDDIR%
 mkdir %BUILDDIR%
 cd %BUILDDIR%
-cmake %PROJDIR% -G"Visual Studio 14 2015 Win64" -DENABLE_TESTING=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_CONFIGURATION_TYPES=Release
+cmake %PROJDIR% -G"Visual Studio 14 2015 Win64" -DBUILD_SHARED_LIBS=ON -DCMAKE_CONFIGURATION_TYPES=Release
 %MSBUILDBIN% ALL_BUILD.vcxproj /property:Configuration=Release
-set PATH=%PROJDIR%build_win64/Release;%PATH%
-ctest .
+set PATH=%PROJDIR%build_win64\Release;%PATH%
+mkdir %OUTPUTDIR%
+copy %BUILDDIR%Release\speck.dll %OUTPUTDIR%
 cd ..
