76d5525ffc52b2f69d314c11230401557e54611f
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-10-02 22:15:49 +0900

    update

--- scripts/speck/build_emscripten.sh
+++ scripts/speck/build_emscripten.sh
@@ -11,12 +11,19 @@ pushd ${BASEDIR} > /dev/null
 # build
 pushd build > /dev/null
 emcmake cmake -DENABLE_STATIC=ON -DCMAKE_BUILD_TYPE=Release ..
-make -j8
+cmake --build .
+
+# rename .c.o to .bc
+ar x libspeck.a
+for f in *.c.o; do
+    mv $f ${f%.c.o}.bc
+done
+zip libspeck.zip *.bc
+
 popd > /dev/null
 
-# create bundle file
 /bin/rm -rf libs/emscripten
 /bin/mkdir -p libs/emscripten
-/bin/cp ./build/libspeck.bc libs/emscripten
+/bin/cp ./build/libspeck.zip libs/emscripten
 
 popd > /dev/null
