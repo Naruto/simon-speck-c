d0dc74fad8b6066b26bdfc206b4334132bdf6e5d
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-11-12 19:46:31 +0900

    support i386 macos

--- scripts/speck/build_mac.sh
+++ scripts/speck/build_mac.sh
@@ -10,7 +10,7 @@ pushd ${BASEDIR} > /dev/null
 /bin/mkdir build
 # build
 pushd build > /dev/null
-cmake -DENABLE_TESTING=ON -DENABLE_AVX2=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" -DENABLE_TESTING=ON -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --clean-first
 ctest .
 popd > /dev/null
